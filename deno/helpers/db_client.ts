9c91a7e7466e741954e2fd96b69345e800f64e9e
Edgar Dias <edgarus@gmail.com>
2020-08-28 12:19:17 +0100

    fix exposed mongodb credentials

--- deno/helpers/db_client.ts
+++ deno/helpers/db_client.ts
@@ -1,11 +1,14 @@
 import { MongoClient, Database } from 'https://deno.land/x/mongo@v0.11.0/mod.ts';
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 let db: Database;
 
+const MONGO_USER = config()['MONGO_USER'];
+const MONGO_PASSWORD = config()['MONGO_PASSWORD'];
+
 export function connect() {
   const client = new MongoClient();
-  client.connectWithUri("mongodb+srv://deno-intro:ykzgu1rxIT7wgan2@cluster0.8zxo2.mongodb.net/?retryWrites=true&w=majority");
-
+  client.connectWithUri(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.8zxo2.mongodb.net/?retryWrites=true&w=majority`);
 
   db = client.database('todo-app');
 }
